fcf9d833d42487f95c47d7b52363814bc200f29d
James Jorissen <jamesjorissen@gmail.com>
2021-07-27 13:56:45 -0500

    javascript updates; folder structure work

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS employeetracker;
+CREATE DATABASE employeetracker;
+
+USE employeetracker;
+
--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function viewDepartments() {
         if (err) throw err;
         console.table(result);
         // re-prompt the user for another selection
-        runSearch();
+        Search();
     }
     );
 };
@@ -114,6 +114,15 @@ function departmentSearch() {
     })
 }
 
+function employeeSearch() {
+    connection.query("SELECT * FROM employee", function (err, data) {
+        if (err) throw err;
+        for (i = 0; i < data.length; i++) {
+            employeeOptions.push(data[i].id + "-" + data[i].first_name + " " + data[i].last_name)
+        }
+    })
+}
+
 function roleSearch() {
     connection.query("SELECT * FROM role", function (err, data) {
         if (err) throw err;
@@ -122,3 +131,104 @@ function roleSearch() {
         }
     })
 }
+
+
+function addAnEmployee() {
+
+    roleSearch()
+    employeeSearch()
+
+    inquirer.prompt([
+        {
+            name: "first_name",
+            type: "input",
+            message: "Enter the employee's first name here: "
+        },
+
+        {
+            name: "last_name",
+            type: "input",
+            message: "Enter the employee's last name here: "
+        },
+        {
+            name: "role",
+            type: "list",
+            message: "Enter the employee's role here: ",
+            choices: roleOptions
+        },
+        {
+            name: "managerInfo",
+            type: "list",
+            message: "Enter the employee's manager here: ",
+            choices: employeeOptions
+        }
+    ]).then(function (answer) {
+        var getId = answer.role.split("-")
+        var getManagerInfoId = answer.managerInfo.split("-")
+        var query =
+            `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+     VALUES ('${answer.first_name}','${answer.last_name}','${getId[0]}','${getManagerInfoId[0]}')`;
+        connection.query(query, function (err, res) {
+            console.log(`new employee ${answer.first_name} ${answer.last_name} added!`)
+        });
+        Search();
+    });
+};
+function addRole() {
+
+    roleSearch()
+    employeeSearch()
+    departmentSearch()
+
+    inquirer.prompt([
+        {
+            name: "role",
+            type: "input",
+            message: "Enter your new role: "
+        },
+
+        {
+            name: "department",
+            type: "list",
+            message: "Please specify this role's department: ",
+            choices: departmentOptions
+        },
+
+        {
+            name: "salary",
+            type: "number",
+            message: "Enter the salary here: "
+        },
+    ]).then(function (answer) {
+        console.log(`${answer.role}`)
+        var getDepartmentId = answer.department.split("-")
+        var query =
+            `INSERT INTO role (title, salary, department_id)
+        VALUES ('${answer.role}','${answer.salary}','${getDepartmentId[0]}')`;
+        connection.query(query, function (err, res) {
+            console.log(`<br>new role ${answer.role} added!`)
+        });
+        Search();
+    });
+};
+
+function addADepartment() {
+    roleSearch()
+    employeeSearch()
+    departmentSearch()
+
+    inquirer.prompt([
+        {
+            name: "dept",
+            type: "input",
+            message: "Enter the department you would like to add:"
+        }
+    ]).then(function (answer) {
+        var query =
+            `INSERT INTO department (name) VALUES ('${answer.department}')`;
+        connection.query(query, function (err, res) {
+            console.log(`new department added: ${answer.department}`)
+        });
+        Search();
+    });
+};
